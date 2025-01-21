9ed23c6db12adf097d393d556889587554189f73
noaMetzger <noaa2152@gmail.com>
2025-01-21 18:50:25 +0200

    g\

--- lesson6.js
+++ lesson6.js
@@ -1,4 +1,5 @@
 use catalog
+
 db.books.find({title:{$in:[/C#/,/Java/,/Python/]}})//1
 db.books.find({isbn:{$nin:[/^19/,1884777384,/0/]}})//2
 db.books.find({$nor:[{isbn:/^19/},{title:/momgo/},{pageCount:{$gt:600}}]})//3
